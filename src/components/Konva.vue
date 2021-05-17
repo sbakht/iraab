74086debe2f4440db88614569181e79aad83b1f4
Saad Bakht <rotk13@gmail.com>
2021-05-16 20:45:39 -0400

    get quadratic connection working

--- src/components/Konva.vue
+++ src/components/Konva.vue
@@ -1,27 +1,52 @@
 <template>
   <v-stage :config="configKonva">
     <v-layer>
-      <v-circle :config="configCircle"></v-circle>
+      <v-circle :config="configCircle" ref="start"></v-circle>
+      <v-circle :config="configCircle2" ref="control"></v-circle>
+      <v-circle :config="configCircle3" ref="end"></v-circle>
+      <v-shape
+        :config="{
+          sceneFunc: mkLine,
+          stroke: 'red',
+          strokeWidth: 4,
+        }"
+      />
     </v-layer>
   </v-stage>
 </template>
 
 <script>
+const width = window.innerWidth;
+const height = window.innerHeight;
 export default {
   data() {
+    const circle = {
+      radius: 70,
+      fill: "red",
+      stroke: "black",
+      strokeWidth: 4,
+      draggable: true,
+    };
     return {
+      quad: {},
       configKonva: {
-        width: 1800,
-        height: 1000,
+        width: width,
+        height: height,
       },
       configCircle: {
+        ...circle,
         x: 100,
         y: 100,
-        radius: 70,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 4,
-        draggable: true,
+      },
+      configCircle2: {
+        ...circle,
+        x: 300,
+        y: 150,
+      },
+      configCircle3: {
+        ...circle,
+        x: 100,
+        y: 300,
       },
     };
   },
@@ -29,6 +54,26 @@ export default {
     // const stage = this.$refs.stage.getNode();
     // const layer = this.$refs.layer.getNode();
     // const rect = this.$refs.rect.getNode();
+    console.log(this.quad);
+    this.quad.start = this.$refs.start.getNode();
+    this.quad.control = this.$refs.control.getNode();
+    this.quad.end = this.$refs.end.getNode();
+  },
+  methods: {
+    mkLine(ctx, shape) {
+      const quad = this.quad;
+      if (this.quad.start) {
+        ctx.beginPath();
+        ctx.moveTo(quad.start.x(), quad.start.y());
+        ctx.quadraticCurveTo(
+          quad.control.x(),
+          quad.control.y(),
+          quad.end.x(),
+          quad.end.y()
+        );
+        ctx.fillStrokeShape(shape);
+      }
+    },
   },
 };
 </script>
